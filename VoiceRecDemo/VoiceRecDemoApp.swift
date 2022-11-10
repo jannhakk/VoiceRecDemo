f84f90787ecc86fd3267381c0565001847e8efd5
Janne Hakkarainen <jannhakk@metropolia.fi>
2022-11-10 10:51:11 +0200

    Initial Commit

---
+++ VoiceRecDemo/VoiceRecDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRecDemoApp.swift
+//  VoiceRecDemo
+//
+//  Created by iosdev on 10.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRecDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
